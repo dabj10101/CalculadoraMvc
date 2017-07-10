a78bf301305cba1bb7bb6d56bbbf12769810f06f
andresbaquero <dajbaquero@udlanet.ec>
2017-07-10 14:47:18 -0500

    first commit

---
+++ CalculadoraWeb/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CalculadoraWeb.Models;
+
+namespace CalculadoraWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        dbCalcEntities db = new dbCalcEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult total(int value1, int value2, String calc)
+        {
+            Resultado r = new Resultado();
+           
+            Calculadora c = new Calculadora();
+            decimal total = 0;
+
+            switch (calc)
+            {
+                case "+":
+                    total = c.suma(value1, value2);
+                        break;
+                case "-":
+                    total = c.resta(value1, value2);
+                    break;
+                case "*":
+                    total = c.multiplicacion(value1, value2);
+                    break;
+                case "/":
+                    total = c.division(value1, value2);
+                    break;
+                default:
+                    total = 0;
+                    break;
+
+            }
+
+            r.Resultados = total;
+            return Content("Resultado:"+total );
+            
+        }
+
+    }
+}
---
+++ CalculadoraWeb/Models/Calculadora.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CalculadoraWeb.Models
+{
+    public class Calculadora
+    {
+        private int a, b;
+
+        public int A
+        {
+            get
+            {
+                return a;
+            }
+
+            set
+            {
+                a = value;
+            }
+        }
+
+        public int B
+        {
+            get
+            {
+                return b;
+            }
+
+            set
+            {
+                b = value;
+            }
+        }
+
+    
+
+        public Calculadora()
+        {
+          
+        }
+
+        public decimal suma(int a, int b)
+        {
+            int total = 0;
+
+            total = a + b;
+
+            return total;
+
+
+        }
+
+        public decimal resta(int a, int b)
+        {
+            int total = 0;
+
+            total = a - b;
+
+            return total;
+
+
+        }
+
+        public decimal multiplicacion(int a, int b)
+        {
+            int total = 0;
+
+            total = a * b;
+
+            return total;
+
+
+        }
+
+        public decimal division(int a, int b)
+        {
+            int total = 0;
+
+            total = a / b;
+
+            return total;
+
+
+        }
+
+
+    }
+}
